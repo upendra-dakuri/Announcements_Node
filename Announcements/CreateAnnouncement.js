cc464a700c765aa149eefd27cf585f496603675c
upendra <bvru.dakuri@gmail.com>
2020-06-04 15:25:04 +0530

    node code

---
+++ Announcements/CreateAnnouncement.js
@@ -0,0 +1,141 @@
+var conn = require('./db');
+var announcement = require('./announcement');
+var jwt = require('jsonwebtoken');
+
+exports.announce = function(req, res){
+
+  jwt.verify(req.token,'secretkey',(err,userdata)=>{
+        
+    if(err){
+      res.sendStatus(403);
+      console.log(err);
+  }else{
+      if(req.file){
+        if(req.body.scheduledTime){
+          var image = req.file.path;
+          var url = image.split('\\');
+          var imageurl = "http://localhost:8081/"+url[1];
+          var tag = JSON.parse(req.body.tags); 
+          announcement.insertMany({title:req.body.title,description:req.body.description,subject: req.body.subject,link:req.body.link,image: imageurl,tags:tag, date:req.body.scheduledTime, isScheduled:true},function(err,data){
+              if(err){
+                  console.log(err);
+                  res.status(500).send("Internal server error ")
+              }else{
+                  res.send(data);
+              }
+          });
+
+      }else{
+          var image = req.file.path;
+          var url = image.split('\\');
+          var imageurl = "http://localhost:8081/"+url[1]; 
+          var tag = JSON.parse(req.body.tags);
+          console.log(req.body.scheduledTime);
+          announcement.insertMany({title:req.body.title,description:req.body.description,subject: req.body.subject,link:req.body.link,tags:tag, image:imageurl, isScheduled:false,date:Date()},function(err,data){
+              if(err){
+                  console.log(err);
+                  res.status(500).send("Internal server error ")
+              }else{
+                  res.send(data);
+              }
+          });
+      } 
+
+    }else{
+      if(req.body.scheduledTime){
+        var tag = JSON.parse(req.body.tags);
+        console.log(req.body.scheduledTime);
+        announcement.insertMany({title:req.body.title, description:req.body.description,subject:req.body.subject, link:req.body.link,tags:tag,date:req.body.scheduledTime,isScheduled:true,image:null}, function(err, data){
+          if(err){
+            console.log(err);
+            res.status(500).send("Internal server error")
+          }else{
+            res.send(data);
+          }
+
+        }); 
+      }else{
+        var tag = JSON.parse(req.body.tags);
+        announcement.insertMany({title:req.body.title, description:req.body.description, subject:req.body.subject, link:req.body.link,tags:tag, image:null, isScheduled:false,date:Date()}, function(err, data){
+           if(err){
+             console.log(err);
+             res.status(500).send("Internal server error");
+
+           }else{
+             res.send(data);
+           }
+
+        });
+       }
+
+      }
+
+    }
+  });
+
+}
+
+exports.getAnnouncement = function(req,res){
+  jwt.verify(req.token, 'secretkey',(err,userdata)=>{
+    if(err){
+      res.sendStatus(403);
+      console.log(err);
+    }else{
+       var currentTime = new Date();
+
+       announcement.find({$or:[{isScheduled:false},{date: { $lte: currentTime }}]}).sort({date:-1}).exec(function(err,records){
+        if(err){
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+
+        }else{
+
+        res.send(records);
+        }
+    });
+  }
+  
+  })
+}
+
+exports.getAnnouncementById =  function(req,res){
+  console.log(req.params.id);
+  announcement.findById(req.params.id,(function(err,data){
+      if(err){
+        
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+      }else{
+          res.send(data);
+
+      }  
+  }));
+
+}
+
+exports.findAnnouncementByTags = function(req, res){
+  console.log(req.token);
+  jwt.verify(req.token,'secretkey', (err, userdata)=>{
+      if(err){
+      res.sendStatus(403);
+      console.log(err);
+      }else{
+        var tag = JSON.parse(req.params.tags);
+        var currentTime = new Date();
+        console.log(tag);
+        announcement.find({$and:[{tags:{$in : tag}},{$or:[{isScheduled:false},{date: { $lte: currentTime }}]}]}).sort({date:-1}).exec(function(err, data){
+        if(err){
+          console.log(err);
+          res.sendStatus(500).send("Internal server error");
+        }else{
+          res.send(data);
+        }
+
+        });
+
+     }
+  })
+
+}
+
+
---
+++ Announcements/UpdateAnnouncement.js
@@ -0,0 +1,52 @@
+var conn = require('./db');
+var announcement = require('./announcement');
+
+exports.update = function(req, res){ 
+
+var updatedRecord = {
+
+    title:req.body.title,
+    subject: req.body.subject,
+    description: req.body.description,
+    link: req.body.link,
+    date:req.body.date
+}
+
+announcement.findByIdAndUpdate(req.params.id, {$set:updatedRecord},{"new":true, upsert:false, useFindAndModify:false}, function(err,result){
+
+       if(err){
+           res.status(500).send("Internal Server Error");
+
+       }else{
+           res.send("Updated record:" +result);
+
+       }
+})
+
+}
+
+exports.delete = function(req, res){
+console.log(req.params.id);
+announcement.findByIdAndRemove(req.params.id,{useFindAndModify:false},function(err, delRecord){
+
+      if(err){
+        res.status(500).status("Internal Server Error");
+
+      }else{
+        res.send("Record deleted"+ delRecord);
+      }
+})
+
+}
+exports.scheduled = function(req,res)
+{
+  announcement.find({'isScheduled':true},function(err, delRecord){
+                  
+    if(err){
+      res.status(500).status("Internal Server Error");
+
+    }else{
+      res.send(delRecord);
+    }
+})
+}
